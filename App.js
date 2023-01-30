b0d8235196f94821dd3a0f5bf83fac3cb87b3a3a
Rudrakshtri642008 <123755886+Rudrakshtri642008@users.noreply.github.com>
2023-01-30 21:10:45 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,36 @@
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+//import { StackNavigator } from '@react-navigation/stack';
+import { createStackNavigator } from '@react-navigation/stack';
+//import { createStackNavigation } from '@react-navigation/stack';
+//import { createStack } from '@react-navigation/stack';
+
+import HomeScreen from "./screens/Home";
+import StartMapScreen from "./screens/StarMap";
+import DailyPicScreen from "./screens/DailyPic";
+import SpaceCraftScreen from "./screens/SpaceCraft";
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home" screenOptions={{
+        headerShown: false
+      }}>
+    
+         <Stack.Screen component="Home" name={HomeScreen} /> 
+         <Stack.Screen name="Home" component={HomeScreen} />
+         <Stack.Screen name="Home" component="HomeScreen" /> 
+        <Stack.Screen name:"Home" component:{HomeScreen} /> 
+    
+        <Stack.Screen name="StarMap" component={StartMapScreen} />
+        <Stack.Screen name="DailyPic" component={DailyPicScreen} />
+        <Stack.Screen name="SpaceCraft" component={SpaceCraftScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
